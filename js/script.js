7b231ac57508578f667764e98f6fd176bac6c2cb
Gabriella Shorr <116584468+GabiShorr@users.noreply.github.com>
2024-02-09 09:28:27 -0500

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,2 +1,14 @@
-
+// Add click event listener to all image links
+document.addEventListener('DOMContentLoaded', function () {
+    var imageLinks = document.querySelectorAll('.image-grid a');
+    imageLinks.forEach(function (link) {
+      link.addEventListener('click', function (event) {
+        // Prevent default behavior (following the link)
+        event.preventDefault();
+        // Redirect to the link specified in the href attribute
+        window.location.href = link.getAttribute('href');
+      });
+    });
+  });
+  
 
